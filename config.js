65730f086497a2ce5d145eb8163482c947c90e68
genericbr <genericbr.business@gmail.com>
2026-05-22 01:02:15 -0300

    Fix formatting of playlistId in config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   //  Como obter a API Key e o Playlist ID: veja README.md
   youtube: {
     apiKey:     "YOUR_YOUTUBE_API_KEY_HERE",
-    playlistId: "list=PLsX_idXfvgUKeFHYUvGJOkwF0mu4nZy_t",
+    playlistId: "PLsX_idXfvgUKeFHYUvGJOkwF0mu4nZy_t",
     maxResults: 50,
   },
 
